1b97c1a6679e806d73c14d1a139c674739d734a9
mfsi.garimas <mfsi.garimas@gmail.com>
2025-10-03 00:13:59 +0530

    fixed: failed to collect data

--- app/(landing)/api/client_logo/[id]/route.ts
+++ app/(landing)/api/client_logo/[id]/route.ts
@@ -3,13 +3,19 @@ import {getById, deleteData} from "@/lib/data/client_logo";
 export async function GET(req: Request, context: { params: Promise<{id: string}> }) {
     const {id} =  await context.params;
     const record = await getById(Number(id));
-    if(!record) return NextResponse.json({error: "Not Found"})
-    return NextResponse.json(record)
+    if (!record) {
+        return NextResponse.json({ error: "Not Found" }, { status: 404 });
+    }
+    
+    return NextResponse.json(record);
 }
 
 export async function DELETE(req: Request, context: { params: Promise<{id: string}> }) {
     const {id} =  await context.params;
     const result = await deleteData(Number(id));
-    if(result) return NextResponse.json({success: true})
-    return NextResponse.json({ success: false, error: 'Failed to remove record' }, { status: 500 })
+    if(result) {
+        return NextResponse.json({success: true})
+    } else {
+        return NextResponse.json({ success: false, error: 'Failed to remove record' }, { status: 500 })
+    }
 }
